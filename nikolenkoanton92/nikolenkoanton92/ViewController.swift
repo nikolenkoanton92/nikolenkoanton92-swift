105a5a55f6db30d005aa02832410d8a26e520bd4
Anton Nikolenko <nikolenkoanton92@gmail.com>
2015-11-28 18:52:32 -0800

    initial skeleton

---
+++ nikolenkoanton92/nikolenkoanton92/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  nikolenkoanton92
+//
+//  Created by Anton Nikolenko on 11/28/15.
+//  Copyright © 2015 Anton Nikolenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
